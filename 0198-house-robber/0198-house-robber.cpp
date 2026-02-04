05a4afe36313f6db2d393769deebcc4fd6e74403
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-02-04 23:33:12 +0530

    Time: 0 ms (100%), Space: 10.7 MB (44.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
+    int maxNonAdjacentSum(int index, vector<int>& nums, vector<int>& dp) {
+        if (index == 0) return nums[0];
+        if (index < 0) return 0;
+
+        if (dp[index] != -1) return dp[index];
+
+        int pick = nums[index] + maxNonAdjacentSum(index-2, nums, dp);
+        int notPick = maxNonAdjacentSum(index-1, nums, dp);
+
+        return dp[index] = max(pick, notPick);
+    }
     int rob(vector<int>& nums) {
         // this can be represented as maximum sum of non-adjacent elements
         int n = nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-
-        vector<int> dp (n);
+        vector<int> dp (n, -1);
         dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
 
-        for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
-        }
-
-        return dp[n-1];
+        return maxNonAdjacentSum(n-1, nums, dp);
     }
 };
