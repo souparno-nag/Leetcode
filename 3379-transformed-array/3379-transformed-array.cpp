c556e7b238c9a7e3742083e80808388313202875
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-02-05 11:34:30 +0530

    Time: 11 ms (18.11%), Space: 25.9 MB (8.08%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                result[i] = nums[(i+nums[i])%n];
+            } else if (nums[i] < 0) {
+                result[i] = nums[((i-abs(nums[i]))%n + n)%n];
+            } else {
+                result[i] = nums[i];
+            }
+        }
+        return result;
+    }
+};
