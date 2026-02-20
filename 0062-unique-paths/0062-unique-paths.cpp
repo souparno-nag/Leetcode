af2ba1dba8e9096bab36fa3f898bf99c385bb912
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-02-20 12:19:53 +0530

    Time: 2 ms (19.95%), Space: 9.5 MB (28.97%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    int countUniquePaths(int i, int j, vector<vector<int>>& dp) {
-        if (i == 0 && j == 0) return dp[i][j] = 1;
-        if (i < 0 || j < 0) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-        int up = countUniquePaths(i - 1, j, dp);
-        int left = countUniquePaths(i, j - 1, dp);
-        return dp[i][j] = up + left;
+    int countUniquePaths(int m, int n) {
+        vector<vector<int>> dp (m, vector<int> (n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    int up = (i > 0) ? dp[i-1][j] : 0;
+                    int left = (j > 0) ? dp[i][j-1] : 0;
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp (m, vector<int> (n, -1));
-        return countUniquePaths(m - 1, n - 1, dp);
+        return countUniquePaths(m , n);
     }
 };
 // Time Complexity = O(M*N)
-// Space Complexity = O((M-1)*(N-1)) {due to recursion stack space} + O(M*N) {dp}
+// Space Complexity = O(M*N) {dp}
