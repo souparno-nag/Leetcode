b7ac3f3cee0e70eecef0c72fbcc2d209fd4c8056
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-02-21 15:19:22 +0530

    Add top-down approach for minimum falling path sum
    Implement top-down dynamic programming solution for minimum falling path sum.

---
+++ 0931-minimum-falling-path-sum/minimum-falling-path-sum-topdown.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMinPathSum(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+        int m = matrix.size(), n = matrix[0].size();
+        if (j < 0 || j >= n) return INT_MAX;
+        if (i == 0) return dp[i][j] = matrix[i][j];
+        if (dp[i][j] != 101) return dp[i][j];
+        int straightUp = findMinPathSum(i - 1, j, matrix, dp);
+        int leftUp = findMinPathSum(i - 1, j - 1, matrix, dp);
+        int rightUp = findMinPathSum(i - 1, j + 1, matrix, dp);
+        return dp[i][j] = min(straightUp, min(leftUp, rightUp)) + matrix[i][j];
+    }
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> dp (m, vector<int> (n, 101));
+        int minPath = INT_MAX;
+        for (int j = 0; j < n; j++) {
+            minPath = min(minPath, findMinPathSum(m-1, j, matrix, dp));
+        }
+        return minPath;
+    }
+};
+// Time Complexity: O(Mx(N^2))
+// Space Complexity: O(MxN) {dp} + O(M) {recursive stack}
