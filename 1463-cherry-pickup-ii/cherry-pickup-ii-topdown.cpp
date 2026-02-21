d02028848d19e1b1b7005fd27df4d3301d268b2a
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-02-22 02:28:42 +0530

    Add cherry pickup II solution with DP
    Implement a solution for the cherry pickup problem using a top-down dynamic programming approach.

---
+++ 1463-cherry-pickup-ii/cherry-pickup-ii-topdown.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxCherries (int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
+        int n = grid.size(), m = grid[0].size();
+        if (j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) return INT_MIN;
+        if (i == n-1) {
+            if (j1 == j2) {
+                return dp[i][j1][j2] = grid[i][j1];
+            } else {
+                return dp[i][j1][j2] = grid[i][j1] + grid[i][j2];
+            }
+        }
+        if (dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        int maxSum = INT_MIN;
+        for (int dj1 = -1; dj1 <= 1; dj1++) {
+            for (int dj2 = -1; dj2 <= 1; dj2++) {
+                maxSum = max(maxSum, maxCherries(i+1, j1+dj1, j2+dj2, grid, dp));
+            }
+        }
+        return dp[i][j1][j2] = maxSum + ((j1 == j2) ? grid[i][j1] : grid[i][j1] + grid[i][j2]);
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>> (m, vector<int> (m, -1)));
+        return maxCherries(0, 0, m-1, grid, dp);
+    }
+};
+// Time Complexity: O(NxMxMx9)
+// Space Complexity: O(NxMxM) {dp} + O(N) {recursive stack space}
