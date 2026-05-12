663ee67a9b723854d301971e46fe75acd10d54a6
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-05-12 15:38:11 +0530

    Time: 0 ms (100%), Space: 15.5 MB (46.24%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,33 +2,24 @@ class Solution {
 public:
     int countWaysToTarget(vector<int>& nums, int target) {
         int n = nums.size();
-        int totalSum = 0;
-        for (int i = 0; i < n; i++) totalSum += nums[i];
-        if (abs(target) > totalSum) return 0;
-        vector<vector<int>> dp(n, vector<int>(2*totalSum+1, 0));
-        int offset = totalSum;
-        // define base case
-        dp[0][offset+nums[0]] += 1;
-        dp[0][offset-nums[0]] += 1;
-        // build the table
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j <= 2 * totalSum; ++j) {
-                if (dp[i - 1][j] != 0) {
-                    int ways = dp[i - 1][j];
-                    // Add nums[i]
-                    int addIdx = j + nums[i];
-                    if (addIdx <= 2 * totalSum)
-                        dp[i][addIdx] += ways;
-                    // Subtract nums[i]
-                    int subIdx = j - nums[i];
-                    if (subIdx >= 0)
-                        dp[i][subIdx] += ways;
-                }
+        vector<vector<int>> dp (n, vector<int> (target+1, 0));
+        dp[0][0] = 1;
+        if (nums[0] <= target) dp[0][nums[0]] += 1;
+        for (int ind = 1; ind < n; ind++) {
+            for (int sum = 0; sum <= target; sum++) {
+                int notPick = dp[ind-1][sum];
+                int pick = (nums[ind] <= sum) ? dp[ind-1][sum-nums[ind]] : 0;
+                dp[ind][sum] = pick + notPick;
             }
         }
-        return dp[n - 1][offset + target];
+        return dp[n - 1][target];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return countWaysToTarget(nums, target);
+        int n = nums.size();
+        int totalSum = 0;
+        for (int i = 0; i < n; i++) totalSum += nums[i];
+        int diff = (totalSum - target);
+        if (diff < 0 || diff%2 != 0) return 0;
+        return countWaysToTarget(nums, diff/2);
     }
 };
