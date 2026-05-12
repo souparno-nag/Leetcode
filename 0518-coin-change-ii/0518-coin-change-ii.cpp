8933db586fad5335830cdaafc4d91952921d077f
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-05-12 16:46:42 +0530

    Time: 34 ms (41.72%), Space: 36.2 MB (63.31%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int countCombinations(vector<vector<int>>& dp, vector<int>& coins, int ind, int target) {
-        if (ind < 0) {
-            return (target == 0) ? 1 : 0;
+    int countCombinations(vector<int>& coins, int target) {
+        int n = coins.size();
+        vector<vector<uint32_t>> dp(n, vector<uint32_t> (target+1, 0));
+        // base case
+        for (int sum = 0; sum <= target; sum++) dp[0][sum] = (sum % coins[0] == 0);
+        // build the table
+        for (int ind = 1; ind < n; ind++) {
+            for (int sum = 0; sum <= target; sum++) {
+                uint32_t notPick = dp[ind-1][sum];
+                uint32_t pick = (coins[ind] <= sum) ? dp[ind][sum-coins[ind]] : 0;
+                dp[ind][sum] = (pick + notPick);
+            }
         }
-        if (dp[ind][target] != -1) return dp[ind][target];
-        int notPick = countCombinations(dp, coins, ind-1, target);
-        int pick = (coins[ind] <= target) ? countCombinations(dp, coins, ind, target-coins[ind]) : 0;
-        return dp[ind][target] = pick + notPick;
+        return (int) dp[n-1][target];
     }
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-        return countCombinations(dp, coins, n-1, amount);
+        return countCombinations(coins, amount);
     }
 };
