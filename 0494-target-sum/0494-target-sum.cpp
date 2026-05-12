fa2dc5a369c61f0283b651d05533dbe27694dc41
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-05-12 15:39:42 +0530

    Time: 426 ms (23.19%), Space: 11.6 MB (83.86%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    int countWaysToTarget(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<vector<int>> dp (n, vector<int> (target+1, 0));
-        dp[0][0] = 1;
-        if (nums[0] <= target) dp[0][nums[0]] += 1;
-        for (int ind = 1; ind < n; ind++) {
-            for (int sum = 0; sum <= target; sum++) {
-                int notPick = dp[ind-1][sum];
-                int pick = (nums[ind] <= sum) ? dp[ind-1][sum-nums[ind]] : 0;
-                dp[ind][sum] = pick + notPick;
-            }
+    int countWaysToTarget(vector<int>& nums, int ind, int sum) {
+        // define base case
+        if (ind == 0) {
+            if ((sum + nums[ind] == 0) && (sum - nums[ind] == 0)) return 2;
+            if (sum + nums[ind] == 0) return 1;
+            else if (sum - nums[ind] == 0) return 1;
+            return 0;
         }
-        return dp[n - 1][target];
+        // define all possibilities
+        int add = countWaysToTarget(nums, ind-1, sum-nums[ind]);
+        int subtract = countWaysToTarget(nums, ind-1, sum+nums[ind]);
+        return add + subtract;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        int totalSum = 0;
-        for (int i = 0; i < n; i++) totalSum += nums[i];
-        int diff = (totalSum - target);
-        if (diff < 0 || diff%2 != 0) return 0;
-        return countWaysToTarget(nums, diff/2);
+        return countWaysToTarget(nums, n-1, target);
     }
 };
