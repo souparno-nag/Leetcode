d8cacab8b745ce5b1be492e67f84a49932494870
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-05-13 16:13:13 +0530

    Time: 26 ms (65.86%), Space: 10.5 MB (77.7%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int countCombinations(vector<int>& coins, int target) {
         int n = coins.size();
-        vector<vector<uint32_t>> dp(n, vector<uint32_t> (target+1, 0));
+        vector<uint32_t> dp(target+1, 0), curr(target+1, 0);
         // base case
-        for (int sum = 0; sum <= target; sum++) dp[0][sum] = (sum % coins[0] == 0);
+        for (int sum = 0; sum <= target; sum++) dp[sum] = (sum % coins[0] == 0);
         // build the table
         for (int ind = 1; ind < n; ind++) {
             for (int sum = 0; sum <= target; sum++) {
-                uint32_t notPick = dp[ind-1][sum];
-                uint32_t pick = (coins[ind] <= sum) ? dp[ind][sum-coins[ind]] : 0;
-                dp[ind][sum] = (pick + notPick);
+                uint32_t notPick = dp[sum];
+                uint32_t pick = (coins[ind] <= sum) ? curr[sum-coins[ind]] : 0;
+                curr[sum] = (pick + notPick);
             }
+            dp = curr;
         }
-        return (int) dp[n-1][target];
+        return (int) dp[target];
     }
     int change(int amount, vector<int>& coins) {
         return countCombinations(coins, amount);
