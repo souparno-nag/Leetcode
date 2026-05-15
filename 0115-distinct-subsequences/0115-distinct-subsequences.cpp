d89f3219562d6a7056ad3481e1fd34555cf92ac7
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-05-15 09:39:36 +0530

    Time: 23 ms (74.86%), Space: 26.7 MB (72.41%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -11,7 +11,20 @@ public:
     }
     int numDistinct(string s, string t) {
         int n = s.length(), m = t.length();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return countDistinctSubsequences(s, t, n-1, m-1, dp);
+        vector<vector<uint32_t>> dp(n+1, vector<uint32_t>(m+1, 0));
+        // base cases
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+        for (int j = 1; j <= m; j++) dp[0][j] = 0;
+        // build the dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i-1] == t[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return (int) dp[n][m];
     }
 };
