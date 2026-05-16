e0abb456da5d488195bfa7a1759263dac2a88961
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-05-16 17:53:00 +0530

    Time: 6 ms (70.83%), Space: 13.1 MB (59.19%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int findMinDistance(string w1, string w2, int i, int j, vector<vector<int>>& dp) {
-        // base case
-        if (j < 0) return i+1;
-        if (i < 0) return j+1;
-        // check dp table
-        if (dp[i][j] != -1) return dp[i][j];
-        // define all possibilities
-        // if characters match
-        if (w1[i] == w2[j]) return dp[i][j] = findMinDistance(w1, w2, i-1, j-1, dp);
-        // if characters do not match
-        int insertion = 1 + findMinDistance(w1, w2, i, j-1, dp);
-        int deletion = 1 + findMinDistance(w1, w2, i-1, j, dp);
-        int replacement = 1 + findMinDistance(w1, w2, i-1, j-1, dp);
-        // return minimum
-        return dp[i][j] = min(replacement, min(insertion, deletion));
-    }
     int minDistance(string word1, string word2) {
         int n = word1.length(), m = word2.length();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return findMinDistance(word1, word2, n-1, m-1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        // define base cases
+        for (int i = 0; i <= n; i++) dp[i][0] = i;
+        for (int j = 0; j <= m; j++) dp[0][j] = j;
+        // build the dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                    continue;
+                }
+                dp[i][j] = 1 + min(
+                    dp[i][j-1], // insert
+                    min(
+                        dp[i-1][j], // delete
+                        dp[i-1][j-1] // replace
+                    )
+                );
+            }
+        }
+        return dp[n][m];
     }
 };
