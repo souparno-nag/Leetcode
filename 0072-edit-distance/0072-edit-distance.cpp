47cca86d1d5689c1b5b4fba984e470f62323292f
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-05-16 17:57:47 +0530

    Time: 0 ms (100%), Space: 10.5 MB (94.59%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         int n = word1.length(), m = word2.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<int> dp(m+1, 0), temp(m+1, 0);
         // define base cases
-        for (int i = 0; i <= n; i++) dp[i][0] = i;
-        for (int j = 0; j <= m; j++) dp[0][j] = j;
+        for (int j = 0; j <= m; j++) dp[j] = j;
         // build the dp table
         for (int i = 1; i <= n; i++) {
+            temp[0] = i;
             for (int j = 1; j <= m; j++) {
                 if (word1[i-1] == word2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1];
+                    temp[j] = dp[j-1];
                     continue;
                 }
-                dp[i][j] = 1 + min(
-                    dp[i][j-1], // insert
+                temp[j] = 1 + min(
+                    temp[j-1], // insert
                     min(
-                        dp[i-1][j], // delete
-                        dp[i-1][j-1] // replace
+                        dp[j], // delete
+                        dp[j-1] // replace
                     )
                 );
             }
+            dp = temp;
         }
-        return dp[n][m];
+        return dp[m];
     }
 };
