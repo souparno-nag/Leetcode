614d9b2d873a9d72af84bdbe15c515ba74f14a45
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-05-18 11:14:05 +0530

    Time: 0 ms (100%), Space: 20 MB (58.61%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,29 +1,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<int> next(2, -1), curr(2, -1);
-        // base case
-        next[0] = next[1] = 0;
-        // build the dp table
-        for (int ind = n-1; ind >= 0; ind--) {
-            for (int buy = 0; buy < 2; buy++) {
-                int profit;
-                if (buy) {
-                    profit = max(
-                        -prices[ind] + next[0], // buy
-                        next[1] // don't buy
-                    ); 
-                } else {
-                    profit = max(
-                        prices[ind] + next[1], // sell
-                        next[0] // don't sell
-                    );
-                }
-                curr[buy] = profit;
-            }
-            next = curr;
+        int profit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > prices[i-1]) profit += (prices[i] - prices[i-1]);
         }
-        return next[1];
+        return profit;
     }
 };
