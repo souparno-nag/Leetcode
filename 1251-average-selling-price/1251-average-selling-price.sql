eb98ec63f333dfc41ad10108541a42068faa5164
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-08-13 23:41:48 +0530

    Time: 965 ms (31.04%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+Select p.product_id,
+coalesce (
+    Round(Sum(p.price * u.units) / Sum(u.units), 2),
+    0
+) as average_price
+from Prices p
+left join UnitsSold u
+on p.product_id = u.product_id
+and u.purchase_date between p.start_date and p.end_date
+group by p.product_id;
