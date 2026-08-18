d208f8a312072786da06a6bc288466ce0d9838da
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-08-18 09:02:03 +0530

    Time: 0 ms (100%), Space: 16.2 MB (53.26%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -20,17 +20,14 @@ public:
         vector<vector<int>> dp(n+2, vector<int>(2, 0));
         for (int ind = n-1; ind >= 0; ind--) {
             for (int buy = 0; buy <= 1; buy++) {
-                if (buy) {
-                    dp[ind][buy] = max(
-                        dp[ind+1][0] - prices[ind],
-                        dp[ind+1][1]
-                    );
-                } else {
-                    dp[ind][buy] = max(
-                        dp[ind+2][1] + prices[ind],
-                        dp[ind+1][0]
-                    );
-                }
+                dp[ind][1] = max(
+                    dp[ind+1][0] - prices[ind],
+                    dp[ind+1][1]
+                );
+                dp[ind][0] = max(
+                    dp[ind+2][1] + prices[ind],
+                    dp[ind+1][0]
+                );
             }
         }
         return dp[0][1];
