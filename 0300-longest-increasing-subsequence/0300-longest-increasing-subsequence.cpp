2d885a66bc0bb2cdc918d631c4f25f0cfec32102
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-08-19 15:20:10 +0530

    Time: 280 ms (32.34%), Space: 296.6 MB (6.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,14 +1,4 @@
 class Solution { 
-    int getLIS(int idx, int prev_idx, vector<int>& nums, int n, vector<vector<int>>& dp) {
-        if (idx == n) return 0;
-        if (dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx+1];
-        int notTake = getLIS(idx+1, prev_idx, nums, n, dp);
-        int take = 0;
-        if (prev_idx == -1 || nums[idx] > nums[prev_idx]) {
-            take = 1 + getLIS(idx+1, idx, nums, n, dp);
-        }
-        return dp[idx][prev_idx+1] = max(notTake, take);
-    }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
