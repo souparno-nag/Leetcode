918bb55c9f0831ff3ed90033bbd8a6483cb02e0a
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-09-09 10:53:12 +0530

    Time: 3 ms (28.71%), Space: 9.9 MB (12.93%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -1,23 +1,17 @@
 class Solution {
-    long long MOD = 1e9+7;
-    long long countDisSub(int i, string s, vector<long long>& dp, vector<long long>& prevIndex) {
-        if (i == -1) return 0;
-        if (dp[i] != -1) return dp[i];
-        int j = prevIndex[i];
-        if (j != -1) return dp[i] =  ((2*countDisSub(i-1, s, dp, prevIndex)%MOD)%MOD - countDisSub(j-1, s, dp, prevIndex) + MOD)%MOD;
-        return dp[i] = ((2*countDisSub(i-1, s, dp, prevIndex)%MOD+1)%MOD - countDisSub(j, s, dp, prevIndex) + MOD)%MOD;
-    }
-    
+    int MOD = 1e9+7;    
 public:
     int distinctSubseqII(string s) {
         int n = s.length();
-        int last[26];
-        fill(last, last+26, -1);
-        vector<long long> dp(n, -1), prevIndex(n, -1);
-        for (int i = 0; i < n; i++) {
-            prevIndex[i] = last[s[i] - 'a'];
-            last[s[i] - 'a'] = i;
+        vector<int> last(26, 0);
+        vector<long> dp(n+1, 0);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int j = last[s[i-1]-'a'];
+            if (j == 0) dp[i] = (2*dp[i-1]+1 - dp[j] + MOD)%MOD;
+            else dp[i] = (2*dp[i-1] - dp[j-1] + MOD)%MOD;
+            last[s[i-1]-'a'] = i;
         }
-        return (int) countDisSub(n-1, s, dp, prevIndex);
+        return dp[n];
     }
 };
