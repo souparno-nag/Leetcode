2c38870f891bc8db5bff10b5cb688a66184eb410
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-09-10 21:07:57 +0530

    Time: 21 ms (12.93%), Space: 23 MB (5.45%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -10,20 +10,20 @@
  * };
  */
 class Solution {
-    vector<int> subTree(TreeNode* node, int* count) {
+    vector<int> subTree(TreeNode* node, int& count) {
         if (node == NULL) return {0, 0};
         vector<int> leftSubTree = subTree(node->left, count);
         vector<int> rightSubTree = subTree(node->right, count);
         int sum = leftSubTree[0] + rightSubTree[0] + node->val;
         int nodeCount = leftSubTree[1] + rightSubTree[1] + 1;
         int avg = sum/nodeCount;
-        if (avg == node->val) (*count)++;
+        if (avg == node->val) count++;
         return {sum, nodeCount};
     }
 public:
     int averageOfSubtree(TreeNode* root) {
         int count = 0;
-        subTree(root, &count);
+        subTree(root, count);
         return count;
     }
 };
